45e16055eba41c8ef60f91386af294117bca1f94
Kialan Pillay <kialanpillay@gmail.com>
2020-06-10 17:30:25 +0200

    Comments

--- socialDistanceShopSampleSolution/GridBlock.java
+++ socialDistanceShopSampleSolution/GridBlock.java
@@ -5,13 +5,24 @@ import java.util.concurrent.atomic.*;
 
 // GridBlock class to represent a block in the shop.
 
+/* 
+This class is modified to include a binary semaphore that functions as an mutual exclusion (mutex) lock.
+The purpose of this lock (the semaphore) is to ensure that only one Customer thread can be assigned a particular GridBlock object
+i.e. only one Customer may occupy a block in the simulation at a time. This is required to satisfy the rules of the simulation
+and enforce pandemic safety rules. This is done by protecting critical sections in the code, where the boolean variable isOccupied
+is read or written to. This is critical in ensuring that multiple Customer threads do not end up being assigned the same GridBlock
+object.
+A binary semaphore is a semaphore that has an integer value that can range only between 0 and 1.
+*/
+
 public class GridBlock {
 	private boolean isOccupied;
 	private final boolean isExit; 
 	private final boolean isCheckoutCounter;
 	private int [] coords; // the coordinate of the block.
 	private int ID;
-	private Semaphore mutex;
+	private Semaphore mutex; //Semaphore variable mutex is declared with the private access modifier
+	//Restricts access to critical sections in the code, where the isOccupied variable is read/written to.
 	
 	public static int classCounter=0;
 	
@@ -21,13 +32,18 @@ public class GridBlock {
 		isOccupied= false;
 		ID=classCounter;
 		classCounter++;
-		mutex = new Semaphore(1);
+		mutex = new Semaphore(1); //Semaphore is constructed with the new keyword, and initialised with a value of 1
+		//This allows for one thread to acquire the mutex lock without blocking.
+		//This is neccessary to ensure that an empty GridBlock can be occupied by a Customer thread.
 	}
 	
 	GridBlock(int x, int y, boolean exitBlock, boolean refreshBlock) throws InterruptedException {
 		this(exitBlock,refreshBlock);
 		coords = new int [] {x,y};
-		mutex = new Semaphore(1);
+		mutex = new Semaphore(1); //Semaphore is constructed with the new keyword, and initialised with a value of 1
+		//This allows for one thread to acquire the mutex lock without blocking. 
+		//This is neccessary to ensure that an empty GridBlock can be occupied by a Customer thread.
+		//One permit is assigned to the Semaphore
 	}
 	
 	//getter
@@ -37,30 +53,44 @@ public class GridBlock {
 	public  int getY() {return coords[1];}
 	
 	//for customer to move to a block
+	//Method is modified to correctly implement its function
+	//Method is called by the shared ShopGrid object when Customer threads invoke move
+	//Method returns a boolean value indicating if the GridBlock is occupied or not
 	public boolean get() throws InterruptedException {
-		mutex.acquire();
-		if(!isOccupied){
-			isOccupied=true;
-			mutex.release();
-			return true;
+		mutex.acquire(); //Thread decrements the Semaphore; blocks if value of Semaphore is negative, otherwise proceeds
+		//If the Semaphore has an avaiable permit, the thread can continue into the critical section
+		//All other threads must wait until the current thread has exited the critical section and released the permit (lock)
+		//Thread has exclusive access to the critical section; there are no data races or data inconsistency
+		if(!isOccupied){ //If statement checks the internal variable, ascertaining if the block is occupied or not
+			isOccupied=true; //If false, the block can be occupied by a customer. isOccupied is set to true.
+			mutex.release(); //The semaphore is incremented, releasing the mutex lock, and unblocking one of the waiting threads
+			return true; //Method returns true and exits
 		}
-		mutex.release();
-		return false;
+		//If the block is occupied, the mutex lock is simply released, so other threads may access the critical section
+		//This ensures liveness, allowing threads to progress in time.
+		mutex.release(); //The semaphore is incremented, releasing the mutex lock, and unblocking one of the waiting threads
+		return false; //Method returns false and exists
 		
 	}
 		
 	//for customer to leave a block
+	//Method is modified to correctly implement its function
+	//Method is called by the shared ShopGrid object when Customer threads invoke move, and exit the particular GridBlock
+	//The block is the free, and can be assigned to another Customer thread.
 	public  void release() throws InterruptedException{
+		//Thread decrements the Semaphore; blocks if value of Semaphore is negative, otherwise proceeds
+		//If the Semaphore has an avaiable permit, the thread can continue into the critical section.
 		mutex.acquire();
-		isOccupied =false;
-		mutex.release();
+		isOccupied =false; //isOccupied is set to false, signifying that the block is free for exclusive occupation
+		mutex.release(); //The semaphore is incremented, releasing the mutex lock, and unblocking one of the waiting threads
 	}
 	
 	//getter
+	//Method not called by external classes, so no need to synchronise.
 	public boolean occupied() {
 		return isOccupied;
 	}
-	
+	//The following accessor methods did not require modification.
 	//getter
 	public  boolean isExit() {
 		return isExit;	
--- socialDistanceShopSampleSolution/PeopleCounter.java
+++ socialDistanceShopSampleSolution/PeopleCounter.java
@@ -1,7 +1,19 @@
 package socialDistanceShopSampleSolution;
 
 //class to keep track of people inside and outside and left shop
+
+/*
+This class is modified to conform to the Java Monitor Pattern.
+All data members are private, and all class methods are synchronised on the object's intrinsic lock.
+This ensures that access to the class is thread-safe and not subject to data races.
+The Java Monitor pattern is an provides a convenient and effective mechanism for process synchronization
+although it reduces the liveness of the program, since threads serially access any method of the class.
+The methods in this class are only called by a single CounterDisplay thread, so the reduced concurrency
+is not an issue in this instance.
+*/
+
 public class PeopleCounter {
+	//All variables are declared with the private access modifier.
 	private int peopleOutSide; //counter for people arrived but not yet in the building
 	private int peopleInside; //people inside the shop
 	private int peopleLeft; //people left the shop
@@ -11,46 +23,75 @@ public class PeopleCounter {
 		peopleOutSide = 0;
 		peopleInside = 0;
 		peopleLeft = 0;
-		maxPeople = max;
+		maxPeople = max; //Corrected code to initialise the internal variable with the argument, instead of 0
 	}
 		
 	//getter
+	//Method signature modifed to contain the synchronized keyword; method is synchronized on the object's own lock.
+	//Access to internal data members is exclusive as only one thread can be active in the Monitor object at a tim.
+	//Variables are safe from data races, and no data inconsistency can occu.
+	//Variables are safe from data races, and no data inconsistency can occur if multiple threads call the accessor.
 	synchronized public int getWaiting() {
 		return peopleOutSide;
 	}
 
 	//getter
+	//Method signature modifed to contain the synchronized keyword; method is synchronized on the object's own lock.
+	//Access to internal data members is exclusive as only one thread can be active in the Monitor object at a time.
+	//Variables are safe from data races, and no data inconsistency can occur.
+	//Variables are safe from data races, and no data inconsistency can occur if multiple threads call the accessor.
 	synchronized public int getInside() {
 		return peopleInside;
 	}
 	
 	//getter
+	//Method signature modifed to contain the synchronized keyword; method is synchronized on the object's own lock.
+	//Access to internal data members is exclusive as only one thread can be active in the Monitor object at a time.
+	//Variables are safe from data races, and no data inconsistency can occur if multiple threads call the accessor.
 	synchronized public int getTotal() {
 		return (peopleOutSide+peopleInside+peopleLeft);
 	}
 
 	//getter
+	//Method signature modifed to contain the synchronized keyword; method is synchronized on the object's own lock.
+	//Access to internal data members is exclusive as only one thread can be active in the Monitor object at a time
+	//Variables are safe from data races, and no data inconsistency can occur if multiple threads call the accessor.
 	synchronized public int getLeft() {
 		return peopleLeft;
 	}
 	
 	//getter
+	//Method signature modifed to contain the synchronized keyword; method is synchronized on the object's own lock.
+	//Access to internal data members is exclusive as only one thread can be active in the Monitor object at a time.
+	//Variables are safe from data races, and no data inconsistency can occur if multiple threads call the accessor.
 	synchronized public int getMax() {
 		return maxPeople;
 	}
 	
 	//getter
+	//Method signature modifed to contain the synchronized keyword; method is synchronized on the object's own lock
+	//Access to internal data members is exclusive as only one thread can be active in the Monitor object at a time
+	//Variables are safe from data races, and no data inconsistency can occur.
+	//peopleOutSide is incremented by a single thread serially; there is no concurrent access
 	synchronized public void personArrived() {
 		peopleOutSide++;
 	}
 	
 	//update counters for a person entering the shop
+	//Method signature modifed to contain the synchronized keyword; method is synchronized on the object's own lock.
+	//Access to internal data members is exclusive as only one thread can be active in the Monitor object at a time.
+	//Variables are safe from data races, and no data inconsistency can occur.
+	//Variables are updated by a single thread serially; there is no concurrent access
 	synchronized public void personEntered() {
 		peopleOutSide--;
 		peopleInside++;
 	}
 
 	//update counters for a person exiting the shop
+	//Method signature modifed to contain the synchronized keyword; method is synchronized on the object's own lock.
+	//Access to internal data members is exclusive as only one thread can be active in the Monitor object at a time.
+	//Variables are safe from data races, and no data inconsistency can occur.
+	//Variables are updated by a single thread serially; there is no concurrent access
 	synchronized public void personLeft() {
 		peopleInside--;
 		peopleLeft++;
@@ -58,6 +99,9 @@ public class PeopleCounter {
 	}
 
 	//reset - not really used
+	//Method signature modifed to contain the synchronized keyword; method is synchronized on the object's own lock.
+	//Access to internal data members is exclusive as only one thread can be active in the Monitor object at a time.
+	//Variables are updated by a single thread serially; there is no concurrent access
 	synchronized public void resetScore() {
 		peopleInside = 0;
 		peopleOutSide = 0;
